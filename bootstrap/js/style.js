72ce2dc9d14b2ab3af0337767eb971e8fe78da17
mrishozuber <mrishozuber@gmail.com>
2022-06-03 19:09:58 +0000

    Add files via upload

---
+++ bootstrap/js/style.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#listSearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
